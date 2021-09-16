ad6c1d13576c214703cb436a4df40a8f5fae975e
Hasan75 <hasanjab14@gmail.com>
2021-09-17 00:11:58 +0600

    ap called

---
+++ js/countries.js
@@ -0,0 +1,11 @@
+const loadCountries = () => {
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data));
+}
+
+const displayCountries = countries => {
+    console.log(countries)
+}
+
+loadCountries()
