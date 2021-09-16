e957239faed0263252643da56ff3c6407f605f3a
Hasan75 <hasanjab14@gmail.com>
2021-09-17 00:19:12 +0600

    using map fuction to display countries

--- js/countries.js
+++ js/countries.js
@@ -5,7 +5,19 @@ const loadCountries = () => {
 }
 
 const displayCountries = countries => {
-    console.log(countries)
+    console.log(countries[0])
+    const countryHTML = countries.map(country => getCountryHTML(country));
+    console.log(countryHTML)
+}
+
+const getCountryHTML = country => {
+    return `
+    <div>
+    <h2>${country.name}</h2>
+    <h3>${country.capital}</h3>
+    <img src= "${country.flag}">
+    </div>
+    `
 }
 
 loadCountries()
