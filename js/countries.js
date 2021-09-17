c483165bca7b45b6ee36f75bcaac4995251b9543
Hasan75 <hasanjab14@gmail.com>
2021-09-17 19:25:10 +0600

    added descructuring

--- js/countries.js
+++ js/countries.js
@@ -7,20 +7,22 @@ const loadCountries = () => {
 }
 
 const displayCountries = countries => {
-    //console.log(countries[0])
+    console.log(countries[0])
     const container = document.getElementById('countries');
     //map function used to do practice, here foreach will be better solution
     const countryHTML = countries.map(country => getCountryHTML(country));
     container.innerHTML = countryHTML.join(' ');
-    //console.log(countryHTML[0])
+    console.log(countryHTML[0])
 }
 
 const getCountryHTML = country => {
+    const {name, nativeName, capital, flag} = country;
     return `
     <div class="country">
-    <h2>${country.name}</h2>
-    <h3>${country.capital}</h3>
-    <img data-aos="zoom-in" src= "${country.flag}">
+    <h2>${name}</h2>
+    <h2>${nativeName}</h2>
+    <h3>Capital: ${capital}</h3>
+    <img data-aos="zoom-in" src= "${flag}">
     </div>
     `
 }
